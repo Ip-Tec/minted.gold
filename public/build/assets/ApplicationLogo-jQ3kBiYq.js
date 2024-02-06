ec35ff8d8e110fa41b962abfc2d5e32afe0b9fc9
innocent peter <otakhorpeter@gmail.com>
2024-02-06 22:17:48 +0100

    bu

--- public/build/assets/ApplicationLogo-jQ3kBiYq.js
+++
@@ -1,8 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-import "@inertiajs/react";
-function ApplicationLogo(props) {
-  return /* @__PURE__ */ jsx("img", { src: "/logo.jpg", ...props });
-}
-export {
-  ApplicationLogo as A
-};
---
+++ public/build/assets/ApplicationLogo-lGIWy4tZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-RWR5emyt.js";function i(o){return r.jsx("img",{src:"/logo.jpg",...o})}export{i as A};

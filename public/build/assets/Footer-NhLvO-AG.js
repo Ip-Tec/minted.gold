1163e523f522fac6aff4570e0c4eb13cce2abc42
innocent peter <otakhorpeter@gmail.com>
2024-02-06 18:37:07 +0100

    js bulid

--- public/build/assets/Footer-NhLvO-AG.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-hJ6yOlL2.js";function r(){return e.jsx("div",{className:"flex items-center text-gray-700 justify-center mt-4 text-center p-2",children:e.jsx("p",{children:"For further support, you may visit the Help Center or contact our customer service team."})})}export{r as default};
